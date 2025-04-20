099187bd7764b1b31630016b6d99cc01e4f10a55
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-19 22:19:25 -0400

    Customize Button

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,7 @@ import { extendTailwindMerge } from "tailwind-merge";
 const twMerge = extendTailwindMerge({
   extend: {
     theme: {
-      text: ["label-md"],
+      text: ["label-md", "action-md"],
     },
   },
 });
