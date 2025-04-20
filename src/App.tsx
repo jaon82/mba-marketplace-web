05e49f4b35d387d313d9af123997844aacdc08d3
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-20 14:50:32 -0400

    Add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from "react-router";
+import { Toaster } from "sonner";
 import "./global.css";
 import { router } from "./routes";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Toaster richColors />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
