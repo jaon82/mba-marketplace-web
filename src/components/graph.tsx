0894f454c6203707db0621fb00d39d8bddf9b53e
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-27 16:46:06 -0400

    Create graph card

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -1,3 +1,32 @@
+import { Calendar04Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
 export default function Graph() {
-  return <div>Graph</div>;
+  return (
+    <Card className="flex-1 bg-white rounded-[20px] border-0">
+      <CardHeader className="flex justify-between">
+        <CardTitle className="font-dm-sans text-title-sm text-gray-500">
+          Visitantes
+        </CardTitle>
+        <CardDescription className="flex gap-2 items-center text-label-sm text-gray-300 uppercase">
+          <HugeiconsIcon
+            icon={Calendar04Icon}
+            size={20}
+            className="text-blue-dark"
+          />
+          <span>26 de junho - 25 de julho</span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Gráfico</p>
+      </CardContent>
+    </Card>
+  );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,7 @@ import { extendTailwindMerge } from "tailwind-merge";
 const twMerge = extendTailwindMerge({
   extend: {
     theme: {
-      text: ["label-md", "action-md"],
+      text: ["label-md", "action-md", "title-sm", "label-sm"],
     },
   },
 });
