f61deb384b63d285b0c778a1ae6cd50b9b19a330
Jean Nunes <jean.nunes@uzzipay.com>
2025-05-15 20:13:27 -0400

    Show avatar

--- src/components/profile-avatar.tsx
+++ src/components/profile-avatar.tsx
@@ -1,10 +1,27 @@
+import { getProfile } from "@/api/get-profile";
 import { ImageUploadIcon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ProfileAvatar() {
+  const { data: profile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  console.log(profile);
+
   return (
     <div className="flex justify-center items-center w-12 h-12 rounded-[10px] cursor-pointer bg-shape text-orange-base">
-      <HugeiconsIcon icon={ImageUploadIcon} size={20} strokeWidth={1.5} />
+      {profile?.seller.avatar ? (
+        <img
+          src={profile.seller.avatar.url}
+          className="w-12 h-12 rounded-[10px] object-cover"
+          alt="Avatar"
+        />
+      ) : (
+        <HugeiconsIcon icon={ImageUploadIcon} size={20} strokeWidth={1.5} />
+      )}
     </div>
   );
 }
